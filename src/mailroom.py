74683be04c1ae8e1a74a0a64993a2a11d5574e82
zachrickert <zachrickert@gmail.com>
2016-08-13 08:51:21 -0700

    Add a clear screen feature.

--- src/mailroom.py
+++ src/mailroom.py
@@ -14,15 +14,19 @@ donor_dict = {
 
 def main_menu():
     """Main func that get executed when run in the cli"""
-    while True:
-        print(welcome_message())
-        user_input = input("Selection: ")
-        if not is_valid_input(user_input, ['1', '2', '3']):
-            continue
-        handle_input(user_input)()
+    try:
+        while True:
+            print(welcome_message())
+            user_input = input("Selection: ")
+            if not is_valid_input(user_input, ['1', '2', '3']):
+                continue
+            handle_input(user_input)()
+    except KeyboardInterrupt:
+        print('\nScript terminated\n')
 
 
 def send_thanks():
+    """Send thank-you note submenu"""
     while True:
         clear_screen()
         donor = input("Enter Donor name, 'list' or 'return': ")
@@ -43,13 +47,14 @@ def send_thanks():
 
 
 def report_donors():
-    """Print the list of all donors in a organized table"""
+    """Report submenu"""
     donor_list = donor_list_by_total(donor_dict)
     print(build_report_table(donor_list))
     press_to_continue()
 
 
 def exit():
+    """Terminate program without error"""
     sys.exit(0)
 
 
@@ -83,12 +88,13 @@ def handle_input(user_input):
 
 
 def generate_thankyou(donor, amount):
+    """Create thank-you not from donor name and amount"""
     return 'Thank you, {0} for your donation of ${1:.2f}.\n'.format(
         donor, round(amount, 2))
 
 
 def build_report_table(donor_list):
-    """Generate a report table from donor list"""
+    """Generates a report table from donor list"""
     top_border = '\n{0}|{1}\n'.format('-' * 24, '-' * 10)
     row_separator = '{0}|{1}\n'.format('-' * 24, '-' * 10)
     header = '{0}{1}|{2}\n'.format('Name', ' ' * 20, 'Total')
@@ -100,6 +106,8 @@ def build_report_table(donor_list):
 
 
 def donor_list_by_total(my_dict):
+    """Returns a sorted list of tuples, each tuple contains donor name and
+    total donated amount"""
     donor_list = []
     for donor in my_dict:
         donor_list.append((donor, sum(my_dict[donor])))
@@ -108,6 +116,7 @@ def donor_list_by_total(my_dict):
 
 
 def validate_donation(my_str):
+    """Check for valid donation amount, should be > 0"""
     try:
         amount = float(my_str)
     except ValueError:
--- src/test_mailroom.py
+++ src/test_mailroom.py
@@ -11,24 +11,40 @@ DONOR_THANKYOU = [
 ]
 
 DONOR_DICT = [
-    ({'a': [1, 2, 3], 'b': [4, 5, 6], 'c':[7, 8, 9]}, [('c', 24), ('b', 15), ('a', 6)]),
-    ({'a': [1], 'b': [2], 'c':[3]}, [('c', 3), ('b', 2), ('a', 1)]),
-    ({'a': [1, 3], 'b': [2, 5], 'c':[]}, [('b', 7), ('a', 4), ('c', 0)])
+    (
+        {'a': [1, 2, 3], 'b': [4, 5, 6], 'c':[7, 8, 9]},
+        [('c', 24), ('b', 15), ('a', 6)]
+    ),
+    (
+        {'a': [1], 'b': [2], 'c':[3]},
+        [('c', 3), ('b', 2), ('a', 1)]
+    ),
+    (
+        {'a': [1, 3], 'b': [2, 5], 'c':[]},
+        [('b', 7), ('a', 4), ('c', 0)]
+    )
 ]
 
-DONATION_AMT = [('1', 1.0), ('0', 0.0), ('a', 0), ('-6', 0), (12, 12.0)]
-
-@pytest.mark.parametrize('donor, amount, message', DONOR_THANKYOU)
-def test_generate_thankyou(donor, amount, message):
-    """Test generate_thankyou func to output correct thankyou message based on
-    donor's name and donation amout"""
-    assert m.generate_thankyou(donor, amount) == message
+DONATION_AMT = [
+    ('1', 1.0),
+    ('0', 0.0),
+    ('a', 0),
+    ('-6', 0),
+    (12, 12.0)
+]
 
 INPUT_FUNCS = [
     ('1', m.send_thanks),
     ('2', m.report_donors),
     ('3', m.exit)
 ]
+
+
+@pytest.mark.parametrize('donor, amount, message', DONOR_THANKYOU)
+def test_generate_thankyou(donor, amount, message):
+    """Test generate_thankyou func to output correct thankyou message based on
+    donor's name and donation amout"""
+    assert m.generate_thankyou(donor, amount) == message
 
 
 @pytest.mark.parametrize('user_input, func', INPUT_FUNCS)
@@ -52,7 +68,7 @@ def test_welcome_message():
 def test_donor_list_by_total(donor_dictionary, result):
     assert m.donor_list_by_total(donor_dictionary) == result
 
+
 @pytest.mark.parametrize('donor_amt, result', DONATION_AMT)
 def test_validate_donation(donor_amt, result):
     assert m.validate_donation(donor_amt) == result
-
